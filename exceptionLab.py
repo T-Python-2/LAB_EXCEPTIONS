336109173336b64d04fc6617d3cfd7f96c7112d3
esrasalam <esra.s.a@outlook.com>
2022-04-25 16:14:13 +0300

    Exeptions Lab Done

---
+++ exceptionLab.py
@@ -0,0 +1,17 @@
+
+## Below you have a code that raises an exception , using what you learned do the following:
+
+def additoin(x, y):
+
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+    except NameError:
+        print ("a variable used in addition operation is undefined")
+    else:
+        print("operation is successful")
+    
+
+
+additoin(10, 20)
