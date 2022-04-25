259de13da0c6efdc758f05096954744786c9b746
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2022-04-25 15:48:51 +0300

    Exceptions Lab

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+    except NameError:
+        print("Name Error Variable is not defined")
+    else:
+        print("the operation is successful")
+
+
+additoin(10, 20)
